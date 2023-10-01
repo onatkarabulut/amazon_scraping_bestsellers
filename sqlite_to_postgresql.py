8f43873f54a4b286141232528da5ab854ed223d8
Onat <97096172+onatkarabulut@users.noreply.github.com>
2023-10-01 19:21:27 +0300

    Update sqlite_to_postgresql.py

--- sqlite_to_postgresql.py
+++ sqlite_to_postgresql.py
@@ -1,79 +1,40 @@
 import sqlite3
 import psycopg2
-from psycopg2 import sql
 
-# Bağlantı bilgilerini bir yapılandırma dosyasından okuyun
-sqlite_config = {
-    'dbname': 'veritabanı.db',
-}
+sqlite_conn = sqlite3.connect('veritabani.db')
+sqlite_cursor = sqlite_conn.cursor()
 
-pg_config = {
-    'dbname': '',
-    'user': '',
-    'password': '',
-    'host': '',
-    'port': '',
-}
+pg_conn = psycopg2.connect(
+    dbname='',
+    user='',
+    password='',
+    host='',
+    port=''
+)
+pg_cursor = pg_conn.cursor()
 
-def create_connection(config):
-    try:
-        conn = psycopg2.connect(**config)
-        return conn
-    except psycopg2.Error as e:
-        print(f"Hata: PostgreSQL bağlantısı oluşturulamadı: {e}")
-        return None
+pg_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS all_products (
+        Link VARCHAR,
+        Urun_adi VARCHAR,
+        Puan REAL,
+        Degerlendirme INTEGER,
+        Fiyatlar REAL
+    )
+''')
 
-def compare_columns(sqlite_cursor, pg_cursor):
-    # Sütunları karşılaştırın ve uyumsuz sütunları listeleyin
-    incompatible_columns = []
-    sqlite_columns = [(col[1], col[2]) for col in sqlite_cursor.execute("PRAGMA table_info(your_table)").fetchall()]
-    pg_columns = pg_cursor.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'your_pg_table'").fetchall()
+sqlite_cursor.execute('SELECT * FROM urunler')
+rows = sqlite_cursor.fetchall()
+
+for row in rows:
+    # Boş dizeleri kontrol edin ve boşlukları None ile değiştirin
+    row = [None if value == '' else value for value in row]
     
-    for sqlite_column, pg_column in zip(sqlite_columns, pg_columns):
-        sqlite_name, sqlite_type = sqlite_column
-        pg_name, pg_type = pg_column
-        if sqlite_name != pg_name or sqlite_type != pg_type:
-            incompatible_columns.append((sqlite_name, sqlite_type, pg_name, pg_type))
-    
-    return incompatible_columns
+    pg_cursor.execute('INSERT INTO amazon_products_scraping (Link, Urun_adi, Puan, Degerlendirme, Fiyatlar) VALUES (%s, %s, %s, %s, %s)', (row[1], row[2], float(row[3]) if row[3] is not None else None, row[4] if row[4] is not None else None, float(row[5]) if row[5] is not None else None))
 
-def main():
-    sqlite_conn = sqlite3.connect(sqlite_config['dbname'])
-    sqlite_cursor = sqlite_conn.cursor()
-    
-    pg_conn = create_connection(pg_config)
-    if pg_conn is None:
-        return
-    
-    pg_cursor = pg_conn.cursor()
+pg_conn.commit()
 
-    incompatible_columns = compare_columns(sqlite_cursor, pg_cursor)
-    
-    if incompatible_columns:
-        print("Uyumsuz sütunlar bulundu:")
-        for col_info in incompatible_columns:
-            print(f"SQLite Sütun: {col_info[0]} ({col_info[1]})")
-            print(f"PostgreSQL Sütun: {col_info[2]} ({col_info[3]})")
-        print("Veri taşıma işlemi yapılamaz. Uyumsuz sütunları düzeltin.")
-    else:
-        print("Veriler uyumlu. Veri taşıma işlemine başlanabilir.")
-        
-        # SQLite'den veri alın
-        sqlite_cursor.execute('SELECT * FROM your_table')
-        rows = sqlite_cursor.fetchall()
-        
-        # PostgreSQL'e veri aktarımı
-        for row in rows:
-            insert_query = sql.SQL('INSERT INTO your_pg_table (column1, column2, column3) VALUES ({}, {}, {})').format(
-                sql.Literal(row[0]), sql.Literal(row[1]), sql.Literal(row[2])
-            )
-            pg_cursor.execute(insert_query)
-        
-        pg_conn.commit()
-        print("Veri aktarımı başarıyla tamamlandı.")
-    
-    sqlite_conn.close()
-    pg_conn.close()
+pg_conn.close()
+sqlite_conn.close()
 
-if __name__ == "__main__":
-    main()
+print("Veri aktarımı tamamlandı.")
